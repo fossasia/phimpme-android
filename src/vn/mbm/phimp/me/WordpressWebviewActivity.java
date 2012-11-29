2a8e82d4beb711e5c0d96b26001902e669493f69
Hon Nguyen <vanhonit@gmail.com>
2012-11-29 16:40:50 +0700

    add joooid lib and create function load joomla account

--- src/vn/mbm/phimp/me/WordpressWebviewActivity.java
+++ src/vn/mbm/phimp/me/WordpressWebviewActivity.java
@@ -15,7 +15,21 @@ public class WordpressWebviewActivity extends Activity{
 		webView = (WebView) findViewById(R.id.wvWordpressIntroduction);
 		webView.getSettings().setJavaScriptEnabled(true);
 
-		String customHtml = "<html><body><h1>Hello, WebView</h1></body></html>";
+		String customHtml = 
+		"<html>"+
+		    "<body style='background:#cfe'>"+
+		        "<center><h2>Wordpress Introduction</h2></center>"+
+		        "<p>"+
+		            "If you want to implement wordpress for Phimpme applicaion, please install some plugin below :"+
+		            "<ol>"+
+		                "<li>Drupal to WP XML_RPC <a href='http://wordpress.org/extend/plugins/drupal-to-wp-xml-rpc/'>http://wordpress.org/extend/plugins/drupal-to-wp-xml-rpc/</a> </li>"+
+		                "<li>Wp-xmlrpc-modernization <a href='http://wordpress.org/extend/plugins/xml-rpc-modernization/'>http://wordpress.org/extend/plugins/xml-rpc-modernization/</a></li>"+
+		                "<li>XML-RPC Extended Media Upload <a href='http://wordpress.org/extend/plugins/xml-rpc-extended-media-upload/'>http://wordpress.org/extend/plugins/xml-rpc-extended-media-upload/</a></li>"+
+		            "</ol>"+
+		            "Then, please access admin page <a href='http://yourdomain/wp-admin/options-writing.php'>http://yourdomain/wp-admin/options-writing.php</a> and check enable XML_RPC"+
+		        "</p>"+
+		    "</body>"+
+		"</html>";
 		webView.loadData(customHtml, "text/html", "UTF-8");
 
 	}
