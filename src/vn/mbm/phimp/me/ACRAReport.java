e8a9450e45fb360643a60c2ff1f9454b06ff898e
Hon Nguyen <vanhonit@gmail.com>
2012-12-10 18:03:41 +0700

    support GEO data and ACRA lib

---
+++ src/vn/mbm/phimp/me/ACRAReport.java
@@ -0,0 +1,23 @@
+package vn.mbm.phimp.me;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import android.app.Application;
+import android.util.Log;
+
+@ReportsCrashes(formKey = "dFRsUzBJSWFKUFc3WmFjaXZab2V0dHc6MQ",
+mode = ReportingInteractionMode.TOAST,
+forceCloseDialogAfterToast = false,
+resToastText = R.string.crash_report_text)
+public class ACRAReport extends Application{
+	 @Override
+	  public void onCreate() {
+	      super.onCreate();
+
+	      // The following line triggers the initialization of ACRA
+	      ACRA.init(this);
+	      Log.e("ACRAPeport","ACRAReport running...");
+	  }
+}
