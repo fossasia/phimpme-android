c5869705f1ffc59d802fe84268c66490aedc8918
pa1pal <pawanpal004@gmail.com>
2016-08-17 17:42:55 +0530

    added upload apk script

--- upload-apk.sh
+++ upload-apk.sh
@@ -9,12 +9,12 @@ cd $HOME
 git config --global user.email "pawanpal004@gmail.com"
 git config --global user.name "pa1pal"
   
-git clone --quiet --branch=uploadapk https://pa1pal:$GITHUB_API_KEY@github.com/pa1pal/phimpme-android uploadapk > /dev/null
-cd uploadapk
+git clone --quiet --branch=master https://pa1pal:$GITHUB_API_KEY@github.com/fossasia/phimpme-android master > /dev/null
+cd master
 cp -Rf $HOME/daily/*  sample-apk/
 git add -f .
   # git remote rm origin
   # git remote add origin https://the-dagger:$GITHUB_API_KEY@github.com/the-dagger/open-event-android
   git add -f .
   git commit -m "Update Sample Apk [skip ci]"
-  git push origin uploadapk > /dev/null
+  git push origin master > /dev/null
