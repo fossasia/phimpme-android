f6e00b758793bf29f78ed6414beabff148737a08
pa1pal <pawanpal004@gmail.com>
2016-08-26 17:19:36 +0530

    Added facebook login

---
+++ app/src/main/java/vn/mbm/phimp/me/MyApplication.java
@@ -0,0 +1,43 @@
+package vn.mbm.phimp.me;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * User: pa1pal
+ * Date: 8/26/16
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "vn.mbm.phimp.me",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
