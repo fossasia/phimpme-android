99b5635ed7fe4f939af33fa930fe24e5410dc3eb
Alejandro Heredia <alex.heredia.barbosa@gmail.com>
2017-01-11 09:26:19 -0500

    Automatically apk update (#28)
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Automatically apk update
    
    Resolving fails
    
    Resolving fails

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-
-mkdir $HOME/daily/
-cd app/build/outputs/apk/
-ls
-cp -R app-debug-unaligned.apk $HOME/daily/
-# go to home and setup git
+#create a new directory that will contain out generated apk
+mkdir $HOME/buildApk/ 
+#copy generated apk from build folder to the folder just created
+cp -R app/build/outputs/apk/app-debug.apk $HOME/buildApk/
+#setup git
 cd $HOME
-git config --global user.email "pawanpal004@gmail.com"
-git config --global user.name "pa1pal"
-  
-git clone --quiet --branch=master https://pa1pal:$GITHUB_API_KEY@github.com/fossasia/phimpme-android master > /dev/null
-cd master
-cp -Rf $HOME/daily/*  sample-apk/
+git config --global user.email "noreply@travis.com"
+git config --global user.name "Travis CI" 
+#clone the repository in the buildApk folder
+git clone --quiet --branch=apk  https://heredroky:$GITHUB_API_KEY@github.com/heredroky/phimpme-android  apk > /dev/null
+cd apk  
+#copy the data in the sample-apk folder in the branch apk
+cp -Rf $HOME/buildApk/* sample-apk/ 
+#add files
 git add -f .
-  # git remote rm origin
-  # git remote add origin https://pa1pal:$GITHUB_API_KEY@github.com/fossasia/phimpme-android
-  git add -f .
-  git commit -m "Update Sample Apk [skip ci]"
-  git push origin master > /dev/null
+#commit and skip the tests
+git commit -m "Travis build pushed [skip ci]"
+#push to the branch apk
+git push -fq origin apk > /dev/null
+echo -e "Apk updated"
