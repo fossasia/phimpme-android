20d7bc02d9131526fb3cc609e7ac9e207da62cfa
Varun Kumar <heysadboy@gmail.com>
2017-03-10 03:49:00 +0530

    Adding map functionality!

---
+++ app/src/main/java/vn/mbm/phimp/me/MapImage.java
@@ -0,0 +1,28 @@
+package vn.mbm.phimp.me;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MapImage extends AppCompatActivity {
+
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_image);
+
+        Bundle bundle = getIntent().getExtras();
+        String path = bundle.getString("path");
+
+        imageView = (ImageView)findViewById(R.id.view_image);
+        displayImage(path);
+    }
+
+    public void displayImage(String path)
+    {
+        imageView.setImageURI(Uri.parse(path));
+    }
+}
---
+++ app/src/main/java/vn/mbm/phimp/me/MapsActivity.java
@@ -0,0 +1,110 @@
+package vn.mbm.phimp.me;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
+            @Override
+            public boolean onMarkerClick(Marker arg0) {
+                Intent i = new Intent(MapsActivity.this, MapImage.class);
+                i.putExtra("path", arg0.getTitle());
+                Log.e("Image", arg0.getTitle());
+                startActivity(i);
+                return true;
+            }
+
+        });
+
+        ArrayList<String> listOfAllImages = getImagesPath(this);
+        int i;
+
+        for (i = 0; i < listOfAllImages.size(); i++) {
+            ExifInterface exif = null;
+            try {
+                exif = new ExifInterface(listOfAllImages.get(i));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            float[] latLong = new float[2];
+            boolean hasLatLong = exif.getLatLong(latLong);
+            if (hasLatLong) {
+                System.out.println("Latitude: " + latLong[0]);
+                System.out.println("Longitude: " + latLong[1]);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(latLong[0], latLong[1]))
+                        .title(listOfAllImages.get(i)));
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latLong[0], latLong[1])));
+            }
+        }
+    }
+
+    public static ArrayList<String> getImagesPath(Activity activity) {
+        Uri uri;
+        ArrayList<String> listOfAllImages = new ArrayList<String>();
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        String PathOfImage = null;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+
+        cursor = activity.getContentResolver().query(uri, projection, null,
+                null, null);
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            PathOfImage = cursor.getString(column_index_data);
+
+            listOfAllImages.add(PathOfImage);
+        }
+        return listOfAllImages;
+    }
+
+
+}
