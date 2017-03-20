2c38909cafb37b5ceb5476e0dc0b712564e19abd
subhankar29 <subhankar29shah@gmail.com>
2017-03-20 19:23:40 +0530

    Added Alert Dialog Box to enable GPS.

--- app/src/main/java/vn/mbm/phimp/me/gallery3d/media/CropImage.java
+++ app/src/main/java/vn/mbm/phimp/me/gallery3d/media/CropImage.java
@@ -61,6 +61,7 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region;
+import android.location.LocationManager;
 import android.media.ExifInterface;
 import android.media.FaceDetector;
 import android.media.MediaScannerConnection;
@@ -288,17 +289,50 @@ public class CropImage extends MonitoredActivity {
 
         txtTags = (EditText) findViewById(R.id.txtUploadPhotoTags);
         btnUseMap = (ImageButton) findViewById(R.id.btnUploadPhotoPutPos);
-		/*btnUseMap.setOnClickListener(new OnClickListener() {
+		btnUseMap.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v)
 			{
-				Intent _itent = new Intent(ctx, UploadMap.class);
+
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                boolean enabled = locationManager
+                        .isProviderEnabled(LocationManager.GPS_PROVIDER);
+                //Checks if GPS is active or not.
+                if (!enabled) {
+                    //Added dialog box.
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            CropImage.this, R.style.AppCompatAlertDialogStyle);
+                    alertDialogBuilder
+                            .setMessage("GPS is disabled in your device. Enable it?")
+                            .setCancelable(false)
+                            .setPositiveButton("Enable GPS",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog,
+                                                            int id) {
+                                            Intent callGPSSettingIntent = new Intent(
+                                                    android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                            CropImage.this.startActivity(callGPSSettingIntent);
+                                        }
+                                    });
+                    alertDialogBuilder.setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = alertDialogBuilder.create();
+                    alert.show();
+
+                }
+            }
+
+				/*Intent _itent = new Intent(ctx, UploadMap.class);
 				_itent.putExtra("latitude", txtLatitude.getText().toString());
 				_itent.putExtra("longitude", txtLongtitude.getText().toString());
-				startActivityForResult(_itent, GET_POSITION_ON_MAP);
-			}
-		});*/
+				startActivityForResult(_itent, GET_POSITION_ON_MAP);*/
+
+		});
         // CR: remove TODO's.
         // TODO: we may need to show this indicator for the main gallery
         // application
