c4df31e37466468a77a2ac318a85d86bb30a05c6
Varun Kumar <heysadboy@gmail.com>
2017-03-20 20:41:05 +0530

    Adding a line at EOF

--- app/src/main/java/vn/mbm/phimp/me/MapFragment.java
+++ app/src/main/java/vn/mbm/phimp/me/MapFragment.java
@@ -12,7 +12,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
