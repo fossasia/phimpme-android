adfa3893829507a946a64c885a96f0273796f1c0
rohanagarwal94 <rohanagarwal94@yahoo.com.sg>
2017-03-21 14:18:27 +0530

    Fixes #116 : Share App feature

--- app/src/main/java/vn/mbm/phimp/me/Settings.java
+++ app/src/main/java/vn/mbm/phimp/me/Settings.java
@@ -35,6 +35,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.paypal.android.MEP.CheckoutButton;
 import com.paypal.android.MEP.PayPal;
@@ -179,6 +180,25 @@ public class Settings extends Fragment
 		ctx = getContext();
 
 		PayPal pp = PayPal.getInstance();
+
+		ImageView shareApp = (ImageView)getView().findViewById(R.id.share_app);
+        shareApp.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,
+                            String.format(getString(R.string.whatsapp_promo_msg_template),
+                                    String.format(getString(R.string.app_share_url),ctx.getPackageName())));
+                    startActivity(shareIntent);
+                }
+                catch (Exception e) {
+                    Toast.makeText(ctx, getString(R.string.error_msg_retry), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 		//create donate button
 		final CheckoutButton donateButton = pp.getCheckoutButton(ctx, PayPal.BUTTON_278x43, CheckoutButton.TEXT_DONATE);
