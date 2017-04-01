63ec3dea532d40039d5466822560a132934766ff
amitiwary999 <tiwarya9@gmail.com>
2017-04-01 21:37:17 +0530

    fixed issue #219 if image is selected and user press back button image is not deselected
    fixed issue #219 if image is selected and user press back button image is not deselected

--- app/src/main/java/vn/mbm/phimp/me/PhimpMe.java
+++ app/src/main/java/vn/mbm/phimp/me/PhimpMe.java
@@ -783,23 +783,29 @@ public class PhimpMe extends AppCompatActivity implements BottomNavigationView.O
     @Override
     public boolean onKeyDown(int keycode, KeyEvent event) {
         if (keycode == KeyEvent.KEYCODE_BACK) {
-            AlertDialog.Builder alertbox = new AlertDialog.Builder(ctx);
-            alertbox.setMessage(getString(R.string.exit_message));
-            alertbox.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                    System.exit(0);
-                }
-            });
-            alertbox.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    //Resume to current process
-                }
-            });
-            alertbox.create().show();
-            
+            newGallery fragment = (newGallery) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+            if (currentScreen == HomeScreenState.GALLERY && !fragment.checkdeletablelist()) {
+                    fragment.onResume();
+                
+            } else {
+                AlertDialog.Builder alertbox = new AlertDialog.Builder(ctx);
+                alertbox.setMessage(getString(R.string.exit_message));
+                alertbox.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                alertbox.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Resume to current process
+                    }
+                });
+                alertbox.create().show();
+
+            }
         }
         return false;
     }
