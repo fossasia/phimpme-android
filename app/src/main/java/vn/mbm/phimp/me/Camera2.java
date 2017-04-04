3c115eb27a1ea824b81ff049d3ac68346b2950e7
Ali Ayub Khan <akhan.bscs15seecs@seecs.edu.pk>
2017-04-04 15:45:44 +0500

    Done

--- app/src/main/java/vn/mbm/phimp/me/Camera2.java
+++ app/src/main/java/vn/mbm/phimp/me/Camera2.java
@@ -950,10 +950,12 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		Log.e("Surface","Change");
 		Camera.Parameters parameters = mCamera.getParameters();
 		int state = Camera2.getState();
-		if(state == 1 )	{ parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF); }
-		else if(state == 2)	{ parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO); }
-		else { parameters.setFlashMode(Camera.Parameters.FLASH_MODE_ON);	}
-
+		if(state == 1 )
+			parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+		else if(state == 2)
+			parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+		else
+			parameters.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
 		try{
 			mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, w, h);
 			if (mSupportFocus.contains(Camera.Parameters.FOCUS_MODE_AUTO))
