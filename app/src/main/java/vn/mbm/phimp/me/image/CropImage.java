270da5883df8e2aaa5360ecbbeb602457f87f93f
Vinay Sajjanapu <vinaysajjanapu@gmail.com>
2017-04-11 19:19:34 +0530

    closes #297 Cropped image quality is very low (#310)
    Closes #297. Crop quality change.

--- app/src/main/java/vn/mbm/phimp/me/image/CropImage.java
+++ app/src/main/java/vn/mbm/phimp/me/image/CropImage.java
@@ -181,15 +181,8 @@ public class CropImage extends MonitoredActivity {
                 Log.d("mSaveUri", mSaveUri.toString());
                 Log.d("p[0]", p[0]);
 
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 4;
-                mBitmap = BitmapFactory.decodeFile(p[0], options);
-                if (mBitmap.getWidth() % 2 != 0 || mBitmap.getHeight() % 2 != 0) {
-                    //bitmap width , height must even
-                    Log.i("CropImage", "mBitmap width or height not even");
-                    mBitmap = Bitmap.createScaledBitmap(mBitmap, mBitmap.getWidth() * 4, mBitmap.getHeight() * 4, false);
+                mBitmap = BitmapFactory.decodeFile(p[0]);
 
-                }
                 modifiedBitmap = flippedImaged = mBitmapSave = mBitmap;
 
                 Log.i("CropImage", "mBitmap Width :" + mBitmap.getWidth() + " mBitmap Height : " + mBitmap.getHeight());
