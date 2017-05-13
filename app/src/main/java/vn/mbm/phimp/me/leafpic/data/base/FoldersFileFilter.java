dbdda61b2f8219ffaf46e6ffcc998d3c9a4de6f5
pa1pal <pawanpal004@gmail.com>
2017-05-14 01:03:10 +0530

    added leafpic code

---
+++ app/src/main/java/vn/mbm/phimp/me/leafpic/data/base/FoldersFileFilter.java
@@ -0,0 +1,14 @@
+package vn.mbm.phimp.me.leafpic.data.base;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * Created by dnld on 24/04/16.
+ */
+public class FoldersFileFilter implements FileFilter {
+    @Override
+    public boolean accept(File pathname) {
+        return pathname.isDirectory();
+    }
+}
