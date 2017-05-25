3fa02b49b231cbb0d4b6437de8e06bd4de4a4fcc
Mohit Manuja <mohitmanuja@users.noreply.github.com>
2017-05-25 20:13:40 +0530

    Updated util class

--- app/src/main/java/vn/mbm/phimp/me/Utilities/Utils.java
+++ app/src/main/java/vn/mbm/phimp/me/Utilities/Utils.java
@@ -1,16 +1,8 @@
 package vn.mbm.phimp.me.Utilities;
 
-import vn.mbm.phimp.me.leafpic.util.ThemeHelper;
-
 /**
  * Created by pa1pal on 23/5/17.
  */
 
 public class Utils {
-
-    ThemeHelper themeHelper;
-
-    public int getPrimaryColor(){
-        return themeHelper.getPrimaryColor();
-    }
 }
