9bb241300d90d7b3b03d77ea8ad087886e49b297
Mohit Manuja <mohitmanuja@users.noreply.github.com>
2017-05-27 00:31:49 +0530

    fix: Improved the bottom naviagtion bar and account activity

--- app/src/main/java/vn/mbm/phimp/me/accounts/AccountsActivity.java
+++ app/src/main/java/vn/mbm/phimp/me/accounts/AccountsActivity.java
@@ -1,9 +1,15 @@
 package vn.mbm.phimp.me.accounts;
 
-import vn.mbm.phimp.me.R;
-import vn.mbm.phimp.me.base.BaseActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class AccountsActivity extends BaseActivity {
+import vn.mbm.phimp.me.R;
+import vn.mbm.phimp.me.base.ThemedActivity;
+import vn.mbm.phimp.me.leafpic.util.ThemeHelper;
+
+public class AccountsActivity extends ThemedActivity {
+    Toolbar toolbar;
+    ThemeHelper themeHelper;
 
     @Override
     public int getContentViewId() {
@@ -14,4 +20,28 @@ public class AccountsActivity extends BaseActivity {
     public int getNavigationMenuItemId() {
         return R.id.navigation_accounts;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setNavigationBarColor(ThemeHelper.getPrimaryColor(this));
+        setStatusBarColor();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        themeHelper = new ThemeHelper(this);
+        setToolbar();
+
+    }
+
+    private void setToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setPopupTheme(themeHelper.getPopupToolbarStyle());
+        toolbar.setBackgroundColor(themeHelper.getPrimaryColor());
+        getSupportActionBar().setTitle(R.string.title_account);
+    }
+
 }
