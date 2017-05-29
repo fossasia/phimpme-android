029a9c857ff74c53a865a18348a65a8d8c3cd0d0
subhankar29 <subhankar29shah@gmail.com>
2017-05-29 22:18:18 +0530

    Added Facebook SDK.

--- app/src/main/java/vn/mbm/phimp/me/editor/editimage/adapter/StickerAdapter.java
+++ app/src/main/java/vn/mbm/phimp/me/editor/editimage/adapter/StickerAdapter.java
@@ -74,8 +74,6 @@ public class StickerAdapter extends RecyclerView.Adapter<ViewHolder> {
         String path = pathList.get(position);
         ImageLoader.getInstance().displayImage("assets://" + path,
                 imageHoler.image, imageOption);
-        String log = null;
-        Log.d(log,"Image Loaded");
         imageHoler.image.setTag(path);
         imageHoler.image.setOnClickListener(mImageClick);
     }
