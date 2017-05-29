b2fa73127dd941c09ceeac8130b8be96e05e2f2d
subhankar29 <subhankar29shah@gmail.com>
2017-05-29 22:44:24 +0530

    Created ShareImage activity.

---
+++ app/src/main/java/vn/mbm/phimp/me/ShareImage.java
@@ -0,0 +1,13 @@
+package vn.mbm.phimp.me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_image);
+    }
+}
