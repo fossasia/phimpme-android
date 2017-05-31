f6b1a53bf43909effd2a6c494f487c1dc3833c30
Pawan Pal <pawanpal004@gmail.com>
2017-05-31 16:34:10 +0530

    fix: shifted editimagelibrary module code to main app (#488)
    * wip:shifted editimagelibrary module code to main app
    
    * fix: ndk configuration in travis
    
    * updated .gitignore

--- app/src/main/java/vn/mbm/phimp/me/Utilities/Utils.java
+++
@@ -1,8 +0,0 @@
-package vn.mbm.phimp.me.Utilities;
-
-/**
- * Created by pa1pal on 23/5/17.
- */
-
-public class Utils {
-}
---
+++ app/src/main/java/vn/mbm/phimp/me/editor/editimage/view/imagezoom/utils/IDisposable.java
@@ -0,0 +1,5 @@
+package vn.mbm.phimp.me.editor.editimage.view.imagezoom.utils;
+
+public interface IDisposable {
+	void dispose();
+}
