b87f08330fbf6f0245a62399266fa448a7210c11
Pawan Pal <pawanpal004@gmail.com>
2017-05-31 16:34:10 +0530

    fix: shifted editimagelibrary module code to main app (#488)
    * wip:shifted editimagelibrary module code to main app
    
    * fix: ndk configuration in travis
    
    * updated .gitignore

---
+++ app/src/main/java/vn/mbm/phimp/me/editor/editimage/view/imagezoom/easing/Easing.java
@@ -0,0 +1,10 @@
+package vn.mbm.phimp.me.editor.editimage.view.imagezoom.easing;
+
+public interface Easing {
+
+	double easeOut(double time, double start, double end, double duration);
+
+	double easeIn(double time, double start, double end, double duration);
+
+	double easeInOut(double time, double start, double end, double duration);
+}
