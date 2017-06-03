dc1d7046c169a1486e8b55b8a6ad10175b8812df
Pawan Pal <pawanpal004@gmail.com>
2017-06-03 10:48:41 +0530

    adding Account Manager screen with user adding feature also configure Kotlin (#520)
    * feat: added account manager activity, configured kotlin

---
+++ app/src/main/java/vn/mbm/phimp/me/accounts/AccountsActivity.kt
@@ -0,0 +1,59 @@
+package vn.mbm.phimp.me.accounts
+
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import vn.mbm.phimp.me.R
+import vn.mbm.phimp.me.base.ThemedActivity
+import vn.mbm.phimp.me.leafpic.util.ThemeHelper
+
+class AccountsActivity : ThemedActivity() {
+
+    private var toolbar: Toolbar? = null
+    private var themeHelper: ThemeHelper? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        themeHelper = ThemeHelper(this)
+        toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        toolbar!!.setPopupTheme(themeHelper!!.getPopupToolbarStyle());
+        toolbar!!.setBackgroundColor(themeHelper!!.getPrimaryColor());
+        getSupportActionBar()!!.setTitle(R.string.title_account);
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_accounts_activity, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_add_account -> {
+                val fragmentManager = supportFragmentManager
+                val accountsPicker = AccountsPickerFragment()
+                accountsPicker.show(fragmentManager, "Accounts Picker")
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    override fun getContentViewId(): Int {
+        return R.layout.activity_accounts
+    }
+
+    override fun getNavigationMenuItemId(): Int {
+        return R.id.navigation_accounts
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setNavigationBarColor(ThemeHelper.getPrimaryColor(this))
+        setStatusBarColor()
+    }
+}
