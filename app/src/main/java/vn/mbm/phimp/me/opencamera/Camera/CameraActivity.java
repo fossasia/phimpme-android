c2261aa3be7b184ca169e8dce0f7658d8663b2c5
Anant Prasad <anantprsd5@gmail.com>
2017-06-09 23:02:05 +0530

    App crash on changing Camera photo location fixed

--- app/src/main/java/vn/mbm/phimp/me/opencamera/Camera/CameraActivity.java
+++ app/src/main/java/vn/mbm/phimp/me/opencamera/Camera/CameraActivity.java
@@ -751,6 +751,7 @@ public class CameraActivity extends BaseActivity implements AudioListener.AudioL
 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
+		updateSaveFolder(MyPreferenceFragment.new_save_location);
 		if( MyDebug.LOG )
 			Log.d(TAG, "onWindowFocusChanged: " + hasFocus);
 		super.onWindowFocusChanged(hasFocus);
@@ -1548,7 +1549,6 @@ public class CameraActivity extends BaseActivity implements AudioListener.AudioL
 				editor.apply();
 
 				this.save_location_history.updateFolderHistory(this.getStorageUtils().getSaveLocation());
-				this.preview.showToast(null, getResources().getString(R.string.changed_save_location) + "\n" + this.applicationInterface.getStorageUtils().getSaveLocation());
 			}
 		}
 	}
