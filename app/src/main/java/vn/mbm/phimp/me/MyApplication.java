e65a2f68fed89bff3380d1955b686fbfa57272b7
Pawan Pal <pawanpal004@gmail.com>
2017-06-11 06:21:41 +0530

    Integrating twitter in account manager (#528)
    * Open Camera photos using phimpme app
    
    * feat: added twitter kit, login button
    
    * shifting button to fragments
    
    * added realmobject accounts model
    
    * feat: added twitter in dialog fragment, invoke callback on Button using
    auth client
    
    - checked on debugger session is created and token and other credentials are coming
    - TODO: saving credentials in Realm database
    
    feat: added twitter in dialog fragment, invoke callback on Button using
    auth client
    
    - checked on debugger session is created and token and other credentials are coming
    - TODO: saving credentials in Realm database
    
    * model added
    
    * bug: realm annotation processor
    
    * feat: data saving on realm database

--- app/src/main/java/vn/mbm/phimp/me/MyApplication.java
+++ app/src/main/java/vn/mbm/phimp/me/MyApplication.java
@@ -2,7 +2,17 @@ package vn.mbm.phimp.me;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
+import com.facebook.stetho.Stetho;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import vn.mbm.phimp.me.leafpic.data.Album;
 import vn.mbm.phimp.me.leafpic.data.HandlingAlbums;
 
@@ -22,7 +32,33 @@ public class MyApplication extends Application {
     public void onCreate() {
         albums = new HandlingAlbums(getApplicationContext());
         applicationContext = getApplicationContext();
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(getString(R.string.twitter_CONSUMER_KEY), getString(R.string.twitter_CONSUMER_SECRET)))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+        /**
+         * Realm initialization
+         */
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name("phimpme.realm")
+                .schemaVersion(1)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
         super.onCreate();
+
+        /**
+         * Stetho initialization
+         */
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
     }
 
     public HandlingAlbums getAlbums() {
