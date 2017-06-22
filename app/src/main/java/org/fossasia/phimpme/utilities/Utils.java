622869ef4c5dc408745053af99908227543924e0
subhankar29 <subhankar29shah@gmail.com>
2017-06-22 11:45:50 +0530

    Checking if instagram is installed or not.

--- app/src/main/java/org/fossasia/phimpme/utilities/Utils.java
+++ app/src/main/java/org/fossasia/phimpme/utilities/Utils.java
@@ -1,8 +1,39 @@
 package org.fossasia.phimpme.utilities;
 
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+
 /**
  * Created by pa1pal on 23/5/17.
  */
 
 public class Utils {
+    public boolean isAppInstalled(String packageName, PackageManager pm) {
+        boolean installed = false;
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            installed = false;
+        }
+        return installed;
+    }
+    public final boolean isInternetOn(ConnectivityManager connec) {
+        // Check for network connections
+        if ( connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED ) {
+
+            // if connected with internet
+            return true;
+
+        } else if (
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+                        connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED  ) {
+            //if not connected to internet
+            return false;
+        }
+        return false;
+    }
 }
