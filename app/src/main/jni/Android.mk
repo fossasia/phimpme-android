41eb4bb80f5b6038fe98373f3fa84d4e2b912c98
Vinay Sajjanapu <vinaysajjanapu@gmail.com>
2017-07-03 23:20:57 +0530

    Integrated opencv4android into Phimpme. (#701)
    * integrated opencv
    
    * added download open cv script to download opencv directly on server from travis

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,12 +2,18 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+OPENCV_INSTALL_MODULES:=on
+OPENCV_CAMERA_MODULES:=on
+OPENCV_LIB_TYPE := STATIC
+include $(LOCAL_PATH)/OpenCV.mk
+
 LOCAL_MODULE    := photoprocessing
 
 LOCAL_CFLAGS := -DANDROID_NDK \
                 -DDISABLE_IMPORTGL
-                
+
 LOCAL_SRC_FILES := bicubic_resize.c bitmap.c blur.c colour_space.c filter.c matrix.c mem_utils.c nanojpeg.c photo_processing.c transform.c tune.c
-LOCAL_LDLIBS    := -lm -llog
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_LDLIBS    += -lm -llog
 
 include $(BUILD_SHARED_LIBRARY)
