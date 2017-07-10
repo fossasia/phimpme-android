3eec80451be88a799d09ebfe695030340ad0b57d
pa1pal <pawanpal004@gmail.com>
2017-07-10 17:39:44 +0530

    added nextcloud android-library and login layout for nextCloud

---
+++ app/src/main/java/org/fossasia/phimpme/NextCloudAuth.java
@@ -0,0 +1,13 @@
+package org.fossasia.phimpme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextCloudAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_cloud_auth);
+    }
+}
