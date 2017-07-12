3272af50257796fff5867d746ea339b4372ba6fe
Vinay Sajjanapu <vinaysajjanapu@gmail.com>
2017-07-13 03:01:34 +0530

    implemented enhance using opencv (#764)

--- app/src/main/jni/main_processing.cpp
+++ app/src/main/jni/main_processing.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "enhance.h"
 
 using namespace std;
 using namespace cv;
@@ -39,6 +40,8 @@ extern "C" {
         }
     }
 
+
+
     JNIEXPORT void JNICALL
     Java_org_fossasia_phimpme_editor_editimage_filter_PhotoProcessing_nativeEnhanceImage(JNIEnv *env,
                                                                                      jclass type,
@@ -53,9 +56,10 @@ extern "C" {
 
         Mat &src = *(Mat*)inpAddr;
         Mat &dst = *(Mat*)outAddr;
-        Mat grey,detected_edges;
+/*        Mat grey,detected_edges;
 
         cvtColor( src, grey, CV_BGR2GRAY );
+
 
         blur( grey, detected_edges, Size(3,3) );
         dst.create( grey.size(), grey.type() );
@@ -65,13 +69,40 @@ extern "C" {
         dst = Scalar::all(0);
 
         src.copyTo( dst, detected_edges);
+*/
 
         switch (mode){
             case 0:
+                adjustBrightness(src, dst, val);
+                break;
+            case 1:
+                adjustContrast(src, dst, val);
+                break;
+            case 2:
+                adjustHue(src, dst, val);
+                break;
+            case 3:
+                adjustSaturation(src, dst, val);
+                break;
+            case 4:
+                adjustTemperature(src, dst, val);
+                break;
+            case 5:
+                adjustTint(src, dst, val);
+                break;
+            case 6:
+                adjustVignette(src, dst, val);
+                break;
+            case 7:
+                adjustSharpness(src, dst, val);
+                break;
+            default:
                 break;
         }
 
     }
+
+
 
 }
 
