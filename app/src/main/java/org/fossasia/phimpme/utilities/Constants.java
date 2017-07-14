83edaf15d81e85d90466b26923d1500b15c89de0
Mohit Manuja <mohitmanuja@users.noreply.github.com>
2017-07-14 23:16:46 +0530

    Added imgur image upload feature (#772)

--- app/src/main/java/org/fossasia/phimpme/utilities/Constants.java
+++ app/src/main/java/org/fossasia/phimpme/utilities/Constants.java
@@ -1,5 +1,6 @@
 package org.fossasia.phimpme.utilities;
 
+
 /**
  * Created by manuja on 14/5/17.
  */
@@ -10,4 +11,15 @@ public class Constants {
     public static int SUCCESS = 0;
     public static int FAIL = 1;
 
+    public static final String TAG = "imgur";
+
+    public static final String RESULTS = "results";
+    public static final String STATUS = "status";
+
+    public static final String OK = "OK";
+
+    public static final String MY_IMGUR_CLIENT_ID = "d8462a4b9fefa01";
+    public static final String headerClient = "Client-ID";
+    public static final String IMGUR_IMAGE_UPLOAD_URL = "https://api.imgur.com/3/image/";
 }
+
