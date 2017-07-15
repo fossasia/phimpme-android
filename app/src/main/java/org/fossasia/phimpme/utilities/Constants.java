9365285a6c38b53a39cb48b8d12da206b8e721e6
Mohit Manuja <manuja.mohit@gmail.com>
2017-07-16 02:08:16 +0530

    Integrated Imgur with authentication

--- app/src/main/java/org/fossasia/phimpme/utilities/Constants.java
+++ app/src/main/java/org/fossasia/phimpme/utilities/Constants.java
@@ -18,8 +18,11 @@ public class Constants {
 
     public static final String OK = "OK";
 
-    public static final String MY_IMGUR_CLIENT_ID = "d8462a4b9fefa01";
+    public static final String MY_IMGUR_CLIENT_ID = "fdbf58e9fa9a58f";
+    public static final String MY_IMGUR_SECRET_ID = "e4f6f03054fcddfa6c5d2c455f9af820273c2f33";
     public static final String headerClient = "Client-ID";
     public static final String IMGUR_IMAGE_UPLOAD_URL = "https://api.imgur.com/3/image/";
+    public static final String IMGUR_LOGIN_URL = "https://api.imgur.com/oauth2/authorize?client_id="+MY_IMGUR_CLIENT_ID+"&response_type=token";
+
 }
 
