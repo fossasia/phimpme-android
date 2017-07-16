12bcc37c4437dad3b0b3256e4865e38a15f60950
Mohit Manuja <mohitmanuja@users.noreply.github.com>
2017-07-17 03:41:43 +0530

    Integrated Imgur with authentication (#794)

---
+++ app/src/main/java/org/fossasia/phimpme/sharedimgur/ImgurAuthActivity.java
@@ -0,0 +1,157 @@
+package org.fossasia.phimpme.sharedimgur;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import org.fossasia.phimpme.R;
+import org.fossasia.phimpme.utilities.BasicCallBack;
+import org.fossasia.phimpme.utilities.Constants;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static org.fossasia.phimpme.utilities.Constants.IMGUR_LOGIN_URL;
+
+/**
+ * Created by manuja on 16/7/17.
+ */
+
+public class ImgurAuthActivity extends Activity {
+    private static final String REDIRECT_URL = "https://org.fossasia.phimpme";
+    static BasicCallBack imgurCallBack;
+    @BindView(R.id.login_parent)
+    View parent;
+    private AlertDialog mAlertBuilder;
+
+    public static void setBasicCallBack(BasicCallBack basicCallBack) {
+        imgurCallBack = basicCallBack;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_login);
+        ButterKnife.bind(this);
+        mAlertBuilder = new AlertDialog.Builder(this).create();
+        mAlertBuilder.setCancelable(false);
+        mAlertBuilder.setTitle(R.string.please_wait_title);
+        View view = getLayoutInflater().inflate(R.layout.view_loading, null);
+        ((TextView) view.findViewById(R.id.messageTextViewFromLoading)).setText(getString(R.string.authenticating_your_app_message));
+        mAlertBuilder.setView(view);
+        mAlertBuilder.show();
+
+
+        WebView imgurWebView = (WebView) findViewById(R.id.twitterLoginWebView);
+        imgurWebView.setBackgroundColor(Color.TRANSPARENT);
+        imgurWebView.loadUrl(IMGUR_LOGIN_URL);
+        imgurWebView.getSettings().setJavaScriptEnabled(true);
+
+        imgurWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                if (url.contains(REDIRECT_URL)) {
+
+                    if (url.contains("/?error=")) {
+                        view.loadUrl(IMGUR_LOGIN_URL);
+                        return true;
+                    }
+                    Log.d("user", url);
+
+                    // We will extract the info from the callback url
+                    splitUrl(url, view);
+                } else {
+                    view.loadUrl(url);
+                }
+
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                if (mAlertBuilder != null) {
+                    mAlertBuilder.cancel();
+                }
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                if (mAlertBuilder != null) {
+                    mAlertBuilder.show();
+                }
+            }
+        });
+    }
+    private void splitUrl(String url, WebView view) {
+        String[] outerSplit = url.split("\\#")[1].split("\\&");
+        String username = null;
+        String accessToken = null;
+        String refreshToken = null;
+        long accessTokenExpiration = 0;
+        int index = 0;
+
+        for (String s : outerSplit) {
+            String[] innerSplit = s.split("\\=");
+
+            switch (index) {
+                // Access Token
+                case 0:
+                    accessToken = innerSplit[1];
+                    break;
+
+                // Access Token Expiration
+                case 1:
+                    long expiresIn = Long.parseLong(innerSplit[1]);
+                    accessTokenExpiration = System.currentTimeMillis() + (expiresIn * DateUtils.SECOND_IN_MILLIS);
+                    break;
+
+                // Token Type, not using
+                case 2:
+                    //NO OP
+                    break;
+
+                // Refresh Token
+                case 3:
+                    refreshToken = innerSplit[1];
+                    break;
+
+                // Username
+                case 4:
+                    username = innerSplit[1];
+                    break;
+                default:
+
+            }
+
+            index++;
+        }
+
+        // Make sure that everything was set
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(accessToken) &&
+                !TextUtils.isEmpty(refreshToken) && accessTokenExpiration > 0) {
+
+            CookieManager.getInstance().removeAllCookie();
+            view.clearHistory();
+            view.clearCache(true);
+            view.clearFormData();
+            imgurCallBack.callBack(Constants.SUCCESS, null);
+//                        setResult(Activity.RESULT_OK, new Intent().putExtra(IMGUR_KEY_LOGGED_IN, true));
+            finish();
+        }
+    }
+}
