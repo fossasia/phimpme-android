77f1fcadcc3112ec73bef0fe69c01290e00194cd
Pawan Pal <pawanpal004@gmail.com>
2017-07-30 00:37:24 +0530

    fix: switch compat color

--- app/src/main/java/org/fossasia/phimpme/share/nextcloud/NextCloudAuth.java
+++ app/src/main/java/org/fossasia/phimpme/share/nextcloud/NextCloudAuth.java
@@ -90,7 +90,7 @@ public class NextCloudAuth extends ThemedActivity implements Button.OnClickListe
         if (email.isEmpty()) {
             hostUrlLayout.setError(getString(R.string.err_msg_host));
             return false;
-        } else if (!email.contains("http://")){
+        } else if (!email.contains("http://") || !email.contains("https://")){
             hostUrlLayout.setError("Please add the protocol http/ https");
             return false;
         } else {
