7baada4aa637a21bd8a8bf1b38f931e15743e992
Anant Prasad <anantprsd5@gmail.com>
2017-07-30 20:25:39 +0530

    Added espresso tests for camera activity and added Idling resource (#905)

---
+++ app/src/androidTest/java/org/fossasia/phimpme/leafpic/activities/ElapsedTimeIdlingResource.java
@@ -0,0 +1,38 @@
+package org.fossasia.phimpme.leafpic.activities;
+
+import android.support.test.espresso.IdlingResource;
+
+/**
+ * Created by anant on 29/7/17.
+ */
+
+public class ElapsedTimeIdlingResource implements IdlingResource {
+    private final long startTime;
+    private final long waitingTime;
+    private ResourceCallback resourceCallback;
+
+    public ElapsedTimeIdlingResource(long waitingTime) {
+        this.startTime = System.currentTimeMillis();
+        this.waitingTime = waitingTime;
+    }
+
+    @Override
+    public String getName() {
+        return ElapsedTimeIdlingResource.class.getName() + ":" + waitingTime;
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        long elapsed = System.currentTimeMillis() - startTime;
+        boolean idle = (elapsed >= waitingTime);
+        if (idle) {
+            resourceCallback.onTransitionToIdle();
+        }
+        return idle;
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+        this.resourceCallback = resourceCallback;
+    }
+}
