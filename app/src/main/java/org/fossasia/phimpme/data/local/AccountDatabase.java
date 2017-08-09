7286580960bc93effdc659179bf8d6e09f07f99a
Pawan Pal <pawanpal004@gmail.com>
2017-08-09 11:56:06 +0530

    verify details and fixed keys

--- app/src/main/java/org/fossasia/phimpme/data/local/AccountDatabase.java
+++ app/src/main/java/org/fossasia/phimpme/data/local/AccountDatabase.java
@@ -16,11 +16,10 @@ public class AccountDatabase extends RealmObject{
 
 
     public enum AccountName {
-        FACEBOOK, TWITTER
+        FACEBOOK, TWITTER, NEXTCLOUD
         //, DRUPAL, WORDPRESS
-        , NEXTCLOUD, PINTEREST, FLICKR
-        , IMGUR, DROPBOX, OWNCLOUD
-        , BOX, TUMBLR, INSTAGRAM, WHATSAPP, GOOGLEPLUS,  OTHERS
+        , PINTEREST, FLICKR, IMGUR, DROPBOX, OWNCLOUD, BOX, TUMBLR
+        , INSTAGRAM, WHATSAPP, MESSENGER ,GOOGLEPLUS, OTHERS
     }
     public static int HIDEINACCOUNTS = 5;
 
--- app/src/main/java/org/fossasia/phimpme/utilities/Constants.java
+++ app/src/main/java/org/fossasia/phimpme/utilities/Constants.java
@@ -15,8 +15,8 @@ public class Constants {
     public static String IMGUR_HEADER_USER = "Bearer";
 
     /*Also add dropbox api key in manifest */
-    public final static String DROPBOX_APP_KEY = "DROPBOX_APP_KEY";
-    public final static String DROPBOX_APP_SECRET = "DROPBOX_APP_SECRET";
+    public final static String DROPBOX_APP_KEY = "7fhwc9meece1m9g";
+    public final static String DROPBOX_APP_SECRET = "m5567c8v5yt1qc1";
 
     public final static String BOX_CLIENT_ID = "tkriyn4662erwjil5e55yq6tcs9hgxjs";
     public final static String BOX_CLIENT_SECRET = "TQuwEKGLbK1YRLT4vmVrtu3RVzNSDZwF";
@@ -24,17 +24,17 @@ public class Constants {
     public final static String FLICKR_API_KEY = "108a1496fb4f27a1fe310bd399f9c900";
     public final static String FLICKR_TOKEN_SECRET = "a3134b9731947689";
 
-    public final static String TWITTER_CONSUMER_KEY = "ENTER-CONSUMER-KEY";
-    public final static String TWITTER_CONSUMER_SECRET = "ENTER-CONSUMER-SECRET";
+    public final static String TWITTER_CONSUMER_KEY = "ewQd2iJOgSHDVJDHzVgt9aYix";
+    public final static String TWITTER_CONSUMER_SECRET = "QkIKAxZI4PZmcOHbEReOe91sFJkJLNF9XchdtAej1xi2OimyZP";
 
-    public final static String TUMBLR_CONSUMER_KEY = "ENTER-CONSUMER-KEY";
-    public final static String TUMBLR_CONSUMER_SECRET = "ENTER-CONSUMER-SECRET";
+    public final static String TUMBLR_CONSUMER_KEY = "gOrriv5kT1wOSnZg0u66vXwmo9phPgeXqBlFKOgVQshS4Xe9qh";
+    public final static String TUMBLR_CONSUMER_SECRET = "kQFgrWbIsJK3WEabkUPBOR9zZwSZuM3ikOgCpAk4bYs8HqsBTQ";
     public final static String CALL_BACK_TUMBLR = "http://phimp.me";
 
-    public static final String MY_IMGUR_CLIENT_ID = "enter client-id here";
+    public static final String MY_IMGUR_CLIENT_ID = "ed4cad8c0ad3739";
     public static final String IMGUR_LOGIN_URL = "https://api.imgur.com/oauth2/authorize?client_id=" + MY_IMGUR_CLIENT_ID + "&response_type=token";
 
-    public static final String PINTEREST_APP_ID = "enter app id";
+    public static final String PINTEREST_APP_ID = "4914573327424567067";
 
     public final static String PACKAGE_FACEBOOK = "com.facebook.katana";
     public final static String PACKAGE_INSTAGRAM = "com.instagram.android";
