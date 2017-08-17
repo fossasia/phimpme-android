52a08a62b693f528cdeb4f7d4f7e8377092b3560
Pawan Pal <pawanpal004@gmail.com>
2017-08-17 21:37:48 +0530

    added database mode for upload history (#1035)

---
+++ app/src/main/java/org/fossasia/phimpme/data/local/UploadHistory.java
@@ -0,0 +1,38 @@
+package org.fossasia.phimpme.data.local;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by pa1pal on 16/08/17.
+ */
+
+public class UploadHistory extends RealmObject{
+
+    String name;
+    String pathname;
+    String datetime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPathname() {
+        return pathname;
+    }
+
+    public void setPathname(String pathname) {
+        this.pathname = pathname;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+}
