4edeed85e149ed20ef84625c23e39ab2071dacce
Aditya Kaushik <adityakaushik471@gmail.com>
2017-10-06 21:51:54 +0530

    Fix #1212 - Add appbar animation (#1239)
    * Fix #1212 - Add appbar animation
    
    * CheckForReveal now boolean

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
@@ -1,5 +1,6 @@
 package org.fossasia.phimpme.gallery.activities;
 
+import android.animation.Animator;
 import android.annotation.TargetApi;
 import android.content.ContentResolver;
 import android.content.ContentUris;
@@ -40,6 +41,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.WindowManager;
 import android.webkit.MimeTypeMap;
 import android.widget.CompoundButton;
@@ -117,6 +119,7 @@ public class LFMainActivity extends SharedMediaActivity {
 
     //To handle all photos/Album conditions
     public boolean all_photos = false;
+    private boolean checkForReveal = true;
     final String REVIEW_ACTION = "com.android.camera.action.REVIEW";
     public static ArrayList<Media> listAll;
     public int size;
@@ -142,6 +145,10 @@ public class LFMainActivity extends SharedMediaActivity {
     private View.OnLongClickListener photosOnLongClickListener = new View.OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
+            if(checkForReveal) {
+                enterReveal();
+                checkForReveal = false;
+            }
             Media m = (Media) v.findViewById(R.id.photo_path).getTag();
             //If first long press, turn on selection mode
             hideNavigationBar();
@@ -162,6 +169,26 @@ public class LFMainActivity extends SharedMediaActivity {
         }
     };
 
+    /**
+     * Helper method for making reveal animation for toolbar when any item is selected by long click.
+     */
+    private void enterReveal() {
+
+        final View toolbar = findViewById(R.id.appbar_toolbar);
+
+        // get the center for the clipping circle
+        int cx = toolbar.getMeasuredWidth() / 2;
+        int cy = toolbar.getMeasuredHeight() / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(toolbar.getWidth(), toolbar.getHeight()) / 2;
+
+        // create the animator for this view
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(toolbar, cx, cy, 5, finalRadius);
+
+        anim.start();
+    }
 
     private int toggleSelectPhoto(Media m) {
         if (m != null) {
@@ -277,6 +304,10 @@ public class LFMainActivity extends SharedMediaActivity {
     private View.OnLongClickListener albumOnLongCLickListener = new View.OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
+            if(checkForReveal) {
+                enterReveal();
+                checkForReveal = false;
+            }
             albumsAdapter.notifyItemChanged(getAlbums().toggleSelectAlbum(((Album) v.findViewById(R.id.album_name).getTag())));
             editMode = true;
             invalidateOptionsMenu();
@@ -848,8 +879,10 @@ public class LFMainActivity extends SharedMediaActivity {
 
     private void updateSelectedStuff() {
         if (albumsMode) {
-            if(getAlbums().getSelectedCount()==0)
+            if(getAlbums().getSelectedCount()==0) {
                 clearOverlay();
+                checkForReveal = true;
+            }
             else
                 appBarOverlay();
             if (editMode)
@@ -867,8 +900,10 @@ public class LFMainActivity extends SharedMediaActivity {
                 });
             }
         } else {
-            if(getAlbum().getSelectedCount()==0)
+            if(getAlbum().getSelectedCount()==0) {
                 clearOverlay();
+                checkForReveal = true;
+            }
             else
                 appBarOverlay();
             if (editMode)
@@ -1791,6 +1826,7 @@ public class LFMainActivity extends SharedMediaActivity {
      */
     @Override
     public void onBackPressed() {
+        checkForReveal = true;
         if(editMode && all_photos)
             clearSelectedPhotos();
         getNavigationBar();
