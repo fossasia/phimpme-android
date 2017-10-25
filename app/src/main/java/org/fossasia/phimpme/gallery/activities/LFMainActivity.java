4993cfda282571898ab2a41736ce787a313c6f1a
nikit19 <nikit.bhandari@gmail.com>
2017-10-25 18:02:23 +0530

    Fix #1405 app does not crash on delete

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
@@ -1339,7 +1339,6 @@ public class LFMainActivity extends SharedMediaActivity {
                         } else new DeletePhotos().execute();
                     }
                 });
-                deleteDialog.show();
                 AlertDialog alertDialogDelete = deleteDialog.create();
                 alertDialogDelete.show();
                 AlertDialogsHelper.setButtonTextColor(new int[]{DialogInterface.BUTTON_POSITIVE, DialogInterface.BUTTON_NEGATIVE}, getAccentColor(), alertDialogDelete);
