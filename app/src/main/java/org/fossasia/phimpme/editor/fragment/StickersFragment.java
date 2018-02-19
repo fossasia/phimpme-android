802f56e6d4948863864e8ad5a076541e5851de50
batbrain7 <kumar.mohit983@gmail.com>
2018-02-19 20:22:54 +0530

    Configured the landscape and portrait files programmatically

--- app/src/main/java/org/fossasia/phimpme/editor/fragment/StickersFragment.java
+++ app/src/main/java/org/fossasia/phimpme/editor/fragment/StickersFragment.java
@@ -1,6 +1,7 @@
 package org.fossasia.phimpme.editor.fragment;
 
 import android.content.res.AssetManager;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -42,6 +43,8 @@ public class StickersFragment extends BaseEditFragment implements View.OnClickLi
     mRecyclerAdapter adapter;
     ImageButton cancel,apply;
 
+    RecyclerView.LayoutManager manager;
+
     public StickersFragment() {
 
     }
@@ -60,8 +63,13 @@ public class StickersFragment extends BaseEditFragment implements View.OnClickLi
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        int orientation = getActivity().getResources().getConfiguration().orientation;
+        if(orientation == Configuration.ORIENTATION_PORTRAIT) {
+            manager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        } else if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            manager = new LinearLayoutManager(getActivity());
+        }
         recyclerView = (RecyclerView) fragmentView.findViewById(R.id.editor_recyclerview);
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
         recyclerView.setLayoutManager(manager);
 
         cancel = (ImageButton)fragmentView.findViewById(R.id.sticker_cancel);
