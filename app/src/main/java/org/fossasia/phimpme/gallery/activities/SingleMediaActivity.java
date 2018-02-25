0945535b7ad4d4d5b7f8999f747bda96d89a3a4a
Dhanraj Sahu <sahudhanraj123@gmail.com>
2018-02-25 23:14:15 +0530

    Fix #1880 App doesn't crash on opening favourites photos in SingleMediaActivity (#1895)

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/SingleMediaActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/SingleMediaActivity.java
@@ -498,7 +498,6 @@ public class SingleMediaActivity extends SharedMediaActivity implements ImageAda
             menu.setGroupVisible(R.id.only_photos_options, true);
         }
         else if(!allPhotoMode && favphotomode){
-            menu.findItem(R.id.action_favourites).setVisible(false);
             menu.findItem(R.id.action_copy).setVisible(false);
             menu.findItem(R.id.action_move).setVisible(false);
         }
