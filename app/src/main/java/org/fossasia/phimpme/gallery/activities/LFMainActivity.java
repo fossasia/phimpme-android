55ab416760e20013ad9be8cbd0424093c45a8393
Dhanraj Sahu <sahudhanraj123@gmail.com>
2018-03-25 22:14:08 +0530

    Fixed #1773 Removed clear_selected option (#1809)

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
@@ -1630,8 +1630,7 @@ public class LFMainActivity extends SharedMediaActivity {
                 }
             });
 
-            menu.findItem(R.id.select_all).setTitle(
-                    getString(getAlbums().getSelectedCount() == albumsAdapter.getItemCount() ? R.string.clear_selected : R.string.select_all));
+            menu.findItem(R.id.select_all).setVisible(getAlbums().getSelectedCount() != albumsAdapter.getItemCount() ? true : false);
             menu.findItem(R.id.ascending_sort_action).setChecked(getAlbums().getSortingOrder() == SortingOrder.ASCENDING);
             switch (getAlbums().getSortingMode()) {
                 case NAME:
@@ -1651,12 +1650,9 @@ public class LFMainActivity extends SharedMediaActivity {
 
         } else {
             getfavouriteslist();
-            menu.findItem(R.id.select_all).setTitle(getString(getAlbum().getSelectedCount() == mediaAdapter
+            menu.findItem(R.id.select_all).setVisible(getAlbum().getSelectedCount() == mediaAdapter
                     .getItemCount() || selectedMedias.size() == size || (selectedMedias.size() == favouriteslist.size
-                    () && fav_photos) ? R
-                    .string
-                    .clear_selected :
-                    R.string.select_all));
+                    () && fav_photos) ? false : true);
             menu.findItem(R.id.ascending_sort_action).setChecked(getAlbum().settings.getSortingOrder() == SortingOrder.ASCENDING);
             switch (getAlbum().settings.getSortingMode()) {
                 case NAME:
@@ -1716,7 +1712,9 @@ public class LFMainActivity extends SharedMediaActivity {
             menu.setGroupVisible(R.id.photos_option_men, false);
             menu.findItem(R.id.all_photos).setVisible(!editMode && !hidden);
             menu.findItem(R.id.search_action).setVisible(!editMode);
+            menu.findItem(R.id.select_all).setVisible(getAlbums().getSelectedCount() != albumsAdapter.getItemCount() ? true : false);
             menu.findItem(R.id.settings).setVisible(false);
+
 
             if (getAlbums().getSelectedCount() >= 1) {
                 if (getAlbums().getSelectedCount() > 1) {
@@ -1750,6 +1748,9 @@ public class LFMainActivity extends SharedMediaActivity {
                 menu.findItem(R.id.album_details).setVisible(false);
                 menu.findItem(R.id.all_photos).setVisible(false);
             }
+            menu.findItem(R.id.select_all).setVisible(getAlbum().getSelectedCount() == mediaAdapter
+                    .getItemCount() || selectedMedias.size() == size || (selectedMedias.size() == favouriteslist.size
+                    () && fav_photos) ? false : true);
         }
 
         togglePrimaryToolbarOptions(menu);
@@ -1765,7 +1766,6 @@ public class LFMainActivity extends SharedMediaActivity {
         menu.findItem(R.id.excludeAlbumButton).setVisible(editMode && !all_photos && albumsMode && !fav_photos);
         menu.findItem(R.id.zipAlbumButton).setVisible(editMode && !all_photos && albumsMode && !fav_photos && !hidden &&
                 getAlbums().getSelectedCount() == 1);
-        menu.findItem(R.id.select_all).setVisible(editMode);
         menu.findItem(R.id.delete_action).setVisible((!albumsMode || editMode) && (!all_photos || editMode) &&
                 (!fav_photos));
         menu.findItem(R.id.hideAlbumButton).setVisible(!all_photos && !fav_photos && getAlbums().getSelectedCount() >
@@ -1826,46 +1826,20 @@ public class LFMainActivity extends SharedMediaActivity {
 
             case R.id.select_all:
                 if (albumsMode) {
-                    //if all albums are already selected, unselect all of them
-                    if (getAlbums().getSelectedCount() == albumsAdapter.getItemCount()) {
-                        editMode = false;
-                        getAlbums().clearSelectedAlbums();
-                    }
-                    // else, select all albums
-                    else getAlbums().selectAllAlbums();
+                    getAlbums().selectAllAlbums();
                     albumsAdapter.notifyDataSetChanged();
                 } else {
                     if (!all_photos && !fav_photos) {
-                        //if all photos are already selected, unselect all of them
-                        if (getAlbum().getSelectedCount() == mediaAdapter.getItemCount()) {
-                            editMode = false;
-                            getAlbum().clearSelectedPhotos();
-                        }
-                        // else, select all photos
-                        else getAlbum().selectAllPhotos();
+                        getAlbum().selectAllPhotos();
                         mediaAdapter.notifyDataSetChanged();
-                    } else if (all_photos && !fav_photos) {
-
-                        if (selectedMedias.size() == size) {
-                            editMode = false;
-                            clearSelectedPhotos();
-                        }
-                        // else, select all photos
-                        else {
+                    } else if(all_photos && !fav_photos){
                             clearSelectedPhotos();
                             selectAllPhotos();
-                        }
                         mediaAdapter.notifyDataSetChanged();
-                    } else if (fav_photos && !all_photos) {
-                        if (selectedMedias.size() == favouriteslist.size()) {
-                            editMode = false;
-                            clearSelectedPhotos();
-                        }
-                        // else, select all photos
-                        else {
+                    }
+                    else if(fav_photos && !all_photos){
                             clearSelectedPhotos();
                             selectAllPhotos();
-                        }
                         Collections.sort(favouriteslist, MediaComparators.getComparator(getAlbum().settings.getSortingMode(),
                                 getAlbum().settings.getSortingOrder()));
                         mediaAdapter.swapDataSet(favouriteslist, true);
