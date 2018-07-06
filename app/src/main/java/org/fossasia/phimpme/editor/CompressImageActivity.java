3f557a969a9a527339849c04f69e4a5e4fe35572
Saurav Vishal <sauravvishal8797@gmail.com>
2018-07-06 19:05:10 +0530

    Fix #2078-Compress option dialog issue fixed.

--- app/src/main/java/org/fossasia/phimpme/editor/CompressImageActivity.java
+++ app/src/main/java/org/fossasia/phimpme/editor/CompressImageActivity.java
@@ -5,6 +5,7 @@ import static org.fossasia.phimpme.utilities.ActivitySwitchHelper.context;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -23,12 +24,14 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import org.fossasia.phimpme.R;
+import org.fossasia.phimpme.base.ThemedActivity;
 import org.fossasia.phimpme.editor.adapter.ListCompressAdapter;
 import org.fossasia.phimpme.editor.utils.FileUtil;
 import org.fossasia.phimpme.editor.view.imagezoom.ImageViewTouch;
 import org.fossasia.phimpme.editor.view.imagezoom.ImageViewTouchBase;
 import org.fossasia.phimpme.gallery.activities.SingleMediaActivity;
 import org.fossasia.phimpme.gallery.data.base.MediaDetailsMap;
+import org.fossasia.phimpme.gallery.util.AlertDialogsHelper;
 
 import java.io.File;
 import java.io.IOException;
@@ -36,7 +39,7 @@ import java.util.ArrayList;
 
 import id.zelory.compressor.Compressor;
 
-public class CompressImageActivity extends AppCompatActivity {
+public class CompressImageActivity extends ThemedActivity {
 
     public String saveFilePath;
     public static final String EXTRA_OUTPUT = "extra_output";
@@ -44,13 +47,14 @@ public class CompressImageActivity extends AppCompatActivity {
     public final int[] cwidth = new int[1];
     public final int[] cheight = new int[1];
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compress_image);
         initView();
         Button size=(Button)findViewById(R.id.size);
+        size.setBackgroundColor(getAccentColor());
         Button dimension=(Button)findViewById(R.id.bypixel);
+        dimension.setBackgroundColor(getAccentColor());
         ImageButton cancel=(ImageButton)findViewById(R.id.edit_cancel);
         size.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,13 +90,16 @@ public class CompressImageActivity extends AppCompatActivity {
 
         LayoutInflater inflater = getLayoutInflater();
         View dialogLayout = inflater.inflate(R.layout.dialog_compresssize, null);
+        TextView title = (TextView) dialogLayout.findViewById(R.id.compress_title);
+        title.setBackgroundColor(getPrimaryColor());
         SeekBar percentsize = (SeekBar) dialogLayout.findViewById(R.id.seekBar);
+        percentsize.getThumb().setColorFilter(getAccentColor(), PorterDuff.Mode.SRC_IN);
         percentsize.setProgress(0);
         final TextView percent=(TextView)dialogLayout.findViewById(R.id.textview2);
         percentsize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                //options of compress by size from 5% to 95%
+                //options of compress by size from 5% to 9;
                 int progress1=95-progress;
                 progress1=progress1-progress1%5;
                 percent.setText(progress1+"%");
@@ -113,13 +120,13 @@ public class CompressImageActivity extends AppCompatActivity {
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         // this is set the view from XML inside AlertDialog
         alert.setView(dialogLayout);
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
-        alert.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+        alert.setPositiveButton(getResources().getString(R.string.set), new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -132,14 +139,14 @@ public class CompressImageActivity extends AppCompatActivity {
     }
 
     private class SaveCompressedImage extends AsyncTask<String, Void, Void>{
-       private ProgressDialog dialog;
+       private ProgressDialog dialog1;
 
         @Override protected void onPreExecute() {
             super.onPreExecute();
-            dialog = new ProgressDialog(context);
-            dialog.setCancelable(false);
-            dialog.setMessage("Saving");
-            dialog.show();
+            dialog1 = new ProgressDialog(context);
+            dialog1.setCancelable(false);
+            dialog1.setMessage("Saving");
+            dialog1.show();
         }
 
         @Override protected Void doInBackground(String... strings) {
@@ -172,7 +179,7 @@ public class CompressImageActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
             String name = saveFilePath.substring(saveFilePath.lastIndexOf("/") + 1);
             FileUtil.albumUpdate(context, FileUtilsCompress.createFolders().getPath() + "/" + name);
-            dialog.dismiss();
+            dialog1.dismiss();
         }
     }
 
@@ -188,6 +195,8 @@ public class CompressImageActivity extends AppCompatActivity {
         int height= Integer.parseInt(ht[0]);
         LayoutInflater inflater = getLayoutInflater();
         final View dialogLayout = inflater.inflate(R.layout.dialog_compresspixel, null);
+        TextView title = (TextView) dialogLayout.findViewById(R.id.compress_title);
+        title.setBackgroundColor(getPrimaryColor());
         //create options of compress in dimensions in multiple of 2
         int awidth=width;
         int aheight=height;
@@ -219,7 +228,7 @@ public class CompressImageActivity extends AppCompatActivity {
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setView(dialogLayout);
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
@@ -227,5 +236,7 @@ public class CompressImageActivity extends AppCompatActivity {
         });
         AlertDialog dialog = alert.create();
         dialog.show();
+        AlertDialogsHelper.setButtonTextColor(new int[]{DialogInterface.BUTTON_POSITIVE, DialogInterface
+                .BUTTON_NEGATIVE}, getAccentColor(), dialog);
     }
 }
