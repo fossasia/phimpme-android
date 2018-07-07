9638bbd95c6d6f42418b5c2d15afde52ffd676fb
Saurav Vishal <sauravvishal8797@gmail.com>
2018-07-07 19:13:59 +0530

    Create uploadapk_updated_script.sh

---
+++ scripts/uploadapk_updated_script.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ "$COMPONENT" == "ASSEMBLE_RELEASE" ]; then
+  
+  set -e
+
+  git config --global user.name "Travis CI"
+  git config --global user.email "noreply@travis.com"
+
+  export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
+  export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
+
+
+  git clone --quiet --branch=apk https://fossasia:$GITHUB_API_KEY@github.com/fossasia/phimpme-android apk > /dev/null
+  cd apk
+  \cp -r ../app/build/outputs/apk/app-debug.apk app-debug.apk
+  \cp -r ../app/build/outputs/apk/app-release-unsigned.apk app-release-unsigned.apk
+
+  # Create a new branch that will contains only latest apk
+  git checkout --orphan temporary
+
+  # Add generated APK
+  git add --all .
+  git commit -am "[Auto] Update Test Apk "
+
+  # Delete current apk branch
+  git branch -D apk
+  # Rename current branch to apk
+  git branch -m apk
+
+  # Force push to origin since histories are unrelated
+  git push origin apk --force --quiet > /dev/null
+  
+fi
+
+exit 0
+
