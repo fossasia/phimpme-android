b88e5da82278155340d66ce4865de035e472c8c9
Saurav Vishal <sauravvishal8797@gmail.com>
2018-07-25 23:12:22 +0530

    Permanent delete option for Trashbin photos.

--- app/src/main/java/org/fossasia/phimpme/trashbin/TrashBinAdapter.java
+++ app/src/main/java/org/fossasia/phimpme/trashbin/TrashBinAdapter.java
@@ -17,6 +17,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -28,6 +29,8 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class TrashBinAdapter extends RecyclerView.Adapter<TrashBinAdapter.ViewHolder> {
 
@@ -46,9 +49,9 @@ public class TrashBinAdapter extends RecyclerView.Adapter<TrashBinAdapter.ViewHo
         return new TrashBinAdapter.ViewHolder(view);
     }
 
-    @Override public void onBindViewHolder(final TrashBinAdapter.ViewHolder holder, int position) {
+    @Override public void onBindViewHolder(final TrashBinAdapter.ViewHolder holder, final int position) {
         if(trashItemsList.size() != 0){
-            TrashBinRealmModel trashBinRealmModel = trashItemsList.get(position);
+            final TrashBinRealmModel trashBinRealmModel = trashItemsList.get(position);
             String date = trashBinRealmModel.getDatetime();
             try {
                 DateFormat format = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
@@ -77,6 +80,12 @@ public class TrashBinAdapter extends RecyclerView.Adapter<TrashBinAdapter.ViewHo
                                     return true;
 
                                 case R.id.delete_permanently:
+                                    if(deletePermanent(trashBinRealmModel)){
+                                        deleteFromRealm(trashItemsList.get(position).getTrashbinpath());
+                                        trashItemsList.remove(position);
+                                        notifyItemRemoved(position);
+                                        notifyItemRangeChanged(position, trashItemsList.size());
+                                    }
                                     return true;
 
                                 default:
@@ -89,6 +98,31 @@ public class TrashBinAdapter extends RecyclerView.Adapter<TrashBinAdapter.ViewHo
                 }
             });
         }
+    }
+
+    private void deleteFromRealm(final String path){
+        Realm realm = Realm.getDefaultInstance();
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override public void execute(Realm realm) {
+                RealmResults<TrashBinRealmModel> trashBinRealmModels = realm.where(TrashBinRealmModel.class).equalTo
+                        ("trashbinpath", path).findAll();
+                trashBinRealmModels.deleteAllFromRealm();
+            }
+        });
+    }
+
+    private boolean deletePermanent(TrashBinRealmModel trashBinRealmModel){
+        boolean succ = false;
+        String path = trashBinRealmModel.getTrashbinpath();
+        File file = new File(Environment.getExternalStorageDirectory() + "/" + ".nomedia");
+        //File file = new File(Environment.getExternalStorageDirectory() + "/" + "TrashBin");
+        if(file.exists()){
+            File file1 = new File(path);
+            if(file1.exists()){
+                succ = file1.delete();
+            }
+        }
+        return succ;
     }
 
     public void updateTrashListItems(List<TrashBinRealmModel> trashList) {
