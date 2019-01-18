3994e8b313f8f833a37230de4ed976f7576726ab
Anunay Rajhans <anunay99@gmail.com>
2019-01-18 20:52:24 +0530

    Corrected(Moved Up).

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/LFMainActivity.java
@@ -283,8 +283,7 @@ public class LFMainActivity extends SharedMediaActivity {
                 if (!editMode) {
                     mediaAdapter.notifyItemChanged(toggleSelectPhoto(m));
                     editMode = true;
-                }
-                else selectAllPhotosUpTo(getImagePosition(m.getPath()),mediaAdapter);
+                } else selectAllPhotosUpTo(getImagePosition(m.getPath()),mediaAdapter);
             } else if (fav_photos && !all_photos) {
                 if (!editMode) {
                     mediaAdapter.notifyItemChanged(toggleSelectPhoto(m));
