3ff2adecb5613a418e5b33739872a15d337d6c2d
Yash Khare <yashsja@gmail.com>
2019-02-19 18:15:10 +0530

    Text highlight is visible in all themes.

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/SingleMediaActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/SingleMediaActivity.java
@@ -1400,8 +1400,10 @@ public class SingleMediaActivity extends SharedMediaActivity implements ImageAda
                 AlertDialog.Builder descriptionDialogBuilder = new AlertDialog.Builder(SingleMediaActivity.this, getDialogStyle());
                 editTextDescription = getDescriptionDialog(SingleMediaActivity.this, descriptionDialogBuilder);
                 editTextDescription.setSelectAllOnFocus(true);
-                editTextDescription.setHighlightColor(ContextCompat.getColor(getApplicationContext(), R.color
-                        .cardview_shadow_start_color));
+                if(getBaseTheme() == ThemeHelper.DARK_THEME || getBaseTheme() == ThemeHelper.AMOLED_THEME){
+                    editTextDescription.setTextColor(R.color.black);
+                    editTextDescription.setHighlightColor(ContextCompat.getColor(getApplicationContext(), R.color.accent_grey));
+                } else editTextDescription.setHighlightColor(ContextCompat.getColor(getApplicationContext(), R.color.cardview_shadow_start_color));
                 editTextDescription.selectAll();
                 editTextDescription.setSingleLine(false);
                 editTextDescription.setHintTextColor(getResources().getColor(R.color.grey, null));
