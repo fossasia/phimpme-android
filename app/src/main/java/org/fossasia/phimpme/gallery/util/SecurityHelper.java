bd98e8f96e0254e588c59d08d578f0def0a2414f
Yash Khare <yashsja@gmail.com>
2019-02-22 23:54:45 +0530

    Made the checkbox color change with change in accent color. (#2545)

--- app/src/main/java/org/fossasia/phimpme/gallery/util/SecurityHelper.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/util/SecurityHelper.java
@@ -232,6 +232,7 @@ public class SecurityHelper {
         final EditText securityAnswer1 = PasswordDialogLayout.findViewById(R.id.security_answer_edittext);
         final EditText securityQuestion = PasswordDialogLayout.findViewById(R.id.security_question_edittext);
         editTextConfirmPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        checkBox.setButtonTintList(ColorStateList.valueOf(activity.getAccentColor()));
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
