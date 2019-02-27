1a6883cc5e64c88b9f47bf0a6d589d07aafd6c58
neel1998 <neellovepainting@gmail.com>
2019-02-27 17:34:47 +0530

    unnecessary imports removed

--- app/src/main/java/org/fossasia/phimpme/gallery/activities/ExcludedAlbumsActivity.java
+++ app/src/main/java/org/fossasia/phimpme/gallery/activities/ExcludedAlbumsActivity.java
@@ -1,13 +1,10 @@
 package org.fossasia.phimpme.gallery.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
