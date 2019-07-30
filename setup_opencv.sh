9ab707e4c13b92816dbbfe8fe4b936ca2bcf8ccb
yashk2000 <yashsja@gmail.com>
2019-07-30 22:02:50 +0530

    !

---
+++ setup_opencv.sh
@@ -0,0 +1,18 @@
+opencv_version="4.0.1"
+download_url="https://github.com/opencv/opencv/releases/download/${opencv_version}/opencv-${opencv_version}-android-sdk.zip"
+mkdir cache/
+wget ${download_url} -O cache/opencv-android-sdk.zip
+
+unzip -qqo ${opencv_sdk_zip} -d opencv_sdk
+cp -r opencv_sdk/OpenCV-android-sdk/sdk/* opencv/
+rm -rf opencv_sdk
+
+mkdir app/src/main/3rdparty
+mkdir app/src/main/jniLibs
+mkdir app/src/main/staticlibs
+mkdir app/src/main/jni/include
+
+mv opencv/native/3rdparty/* app/src/main/3rdparty
+mv opencv/native/libs/* app/src/main/jniLibs
+mv opencv/native/staticlibs/* app/src/main/staticlibs
+mv opencv/native/jni/include/* app/src/main/jni/include
