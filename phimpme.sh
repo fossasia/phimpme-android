479b38e99ccb10ed3c8664b840d19bdc157082d1
yashk2000 <yashsja@gmail.com>
2019-07-30 13:31:36 +0530

    Files paths

--- phimpme.sh
+++ phimpme.sh
@@ -1,5 +1,5 @@
-mkdir $HOME/openCV/
-cd $HOME/openCV
+mkdir openCV/
+cd openCV
 wget https://github.com/opencv/opencv/releases/download/4.0.1/opencv-4.0.1-android-sdk.zip
 
 unzip opencv-4.0.1-android-sdk.zip
@@ -7,14 +7,14 @@ rm -rf opencv-4.0.1-android-sdk.zip
 # Rename folder name to opencv
 mv OpenCV-android-sdk opencv
 
-cd $TRAVIS_BUILD_DIR
+cd ..
 
-mkdir app/src/main/3rdparty
-mkdir app/src/main/jniLibs
-mkdir app/src/main/staticlibs
-mkdir app/src/main/jni/include
+mkdir phimpme-android/app/src/main/3rdparty
+mkdir phimpme-android/app/src/main/jniLibs
+mkdir phimpme-android/app/src/main/staticlibs
+mkdir phimpme-android/app/src/main/jni/include
 
-mv $HOME/openCV/opencv/sdk/native/3rdparty/* app/src/main/3rdparty
-mv $HOME/openCV/opencv/sdk/native/libs/* app/src/main/jniLibs
-mv $HOME/openCV/opencv/sdk/native/staticlibs/* app/src/main/staticlibs
-mv $HOME/openCV/opencv/sdk/native/jni/include/* app/src/main/jni/include
+mv openCV/opencv/sdk/native/3rdparty/* phimpme-android/app/src/main/3rdparty
+mv openCV/opencv/sdk/native/libs/* phimpme-android/app/src/main/jniLibs
+mv openCV/opencv/sdk/native/staticlibs/* phimpme-android/app/src/main/staticlibs
+mv openCV/opencv/sdk/native/jni/include/* phimpme-android/app/src/main/jni/include
