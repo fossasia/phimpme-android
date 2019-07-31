1cc7a7e5af7041a632f59294c8a9d5e0a288ceca
yashk2000 <yashsja@gmail.com>
2019-07-31 21:23:24 +0530

    Simplified copying process

--- setup_opencv.sh
+++ setup_opencv.sh
@@ -4,14 +4,14 @@ download_url="https://github.com/opencv/opencv/releases/download/${opencv_versio
 mkdir -p cache/
 wget ${download_url} -c -O cache/opencv-android-sdk.zip
 
-unzip -qqo ${opencv_sdk_zip} -d opencv_sdk
+unzip -qqo ${opencv_sdk_zip}
 
 mkdir app/src/main/3rdparty
 mkdir app/src/main/jniLibs
 mkdir app/src/main/staticlibs
 mkdir app/src/main/jni/include
 
-mv opencv_sdk/OpenCV-android-sdk/sdk/native/3rdparty/* app/src/main/3rdparty
-mv opencv_sdk/OpenCV-android-sdk/sdk/native/libs/* app/src/main/jniLibs
-mv opencv_sdk/OpenCV-android-sdk/sdk/native/staticlibs/* app/src/main/staticlibs
-mv opencv_sdk/OpenCV-android-sdk/sdk/native/jni/include/* app/src/main/jni/include
+mv OpenCV-android-sdk/sdk/native/3rdparty/* app/src/main/3rdparty
+mv OpenCV-android-sdk/sdk/native/libs/* app/src/main/jniLibs
+mv OpenCV-android-sdk/sdk/native/staticlibs/* app/src/main/staticlibs
+mv OpenCV-android-sdk/sdk/native/jni/include/* app/src/main/jni/include
