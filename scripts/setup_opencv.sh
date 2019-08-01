a661dd1e1e3fd16c6f6ad5888f09470531c6e158
Yash Khare <yashsja@gmail.com>
2019-08-01 21:50:40 +0530

    chore: Made a single scripts for setting up OpenCV (#2840)
    * New script made
    
    * Updated script
    
    * Checking with travis
    
    * Removed the permission denied error
    
    * Files paths
    
    * Update phimpme.sh
    
    * Removed unused scripts
    
    * !
    
    * Added missing parameter
    
    * Created folder
    
    * Minor changes
    
    * Simplified copying process
    
    * Moved folder to scripts and added cache to travis cache

---
+++ scripts/setup_opencv.sh
@@ -0,0 +1,17 @@
+opencv_version="4.0.1"
+opencv_sdk_zip="cache/opencv-android-sdk.zip"
+download_url="https://github.com/opencv/opencv/releases/download/${opencv_version}/opencv-${opencv_version}-android-sdk.zip"
+mkdir -p cache/
+wget ${download_url} -c -O cache/opencv-android-sdk.zip
+
+unzip -qqo ${opencv_sdk_zip}
+
+mkdir app/src/main/3rdparty
+mkdir app/src/main/jniLibs
+mkdir app/src/main/staticlibs
+mkdir app/src/main/jni/include
+
+mv OpenCV-android-sdk/sdk/native/3rdparty/* app/src/main/3rdparty
+mv OpenCV-android-sdk/sdk/native/libs/* app/src/main/jniLibs
+mv OpenCV-android-sdk/sdk/native/staticlibs/* app/src/main/staticlibs
+mv OpenCV-android-sdk/sdk/native/jni/include/* app/src/main/jni/include
